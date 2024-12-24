f2a8932c905606e2a95809505aaf91b3a7ac49b9
SebastianCastellanosUdeA <102270541+SebastianCastellanosUdeA@users.noreply.github.com>
2024-12-24 04:24:34 -0300

    actualization - maps 3 variables

--- mapa_diario.py
+++ mapa_diario.py
@@ -14,48 +14,91 @@ import cv2
 import os
 import glob
 
-import imageio.v2 as imageio
-
 # Carga los polígonos de los barrios
 barrios = gpd.read_file("BairrosFortal.gpkg")
 
-datos_pm25 = pd.read_excel("powevi.xlsx")
-datos_pm25 = pd.read_excel("powerviprueba.xlsx")
+datos = pd.read_excel("powevi.xlsx")
+datos = pd.read_excel("consolidado variaveis.xlsx")
 
-datos_pm25['fecha'] = pd.to_datetime(datos_pm25['data'])
-
+datos['fecha'] = pd.to_datetime(datos['data'])
+datos['ano'] = pd.to_numeric(datos['ano'])
 # Asegúrate de que los nombres de los barrios coincidan en ambos dataframes
-barrios = barrios.merge(datos_pm25, how="left", left_on="nome", right_on="bairro")
+barrios = barrios.merge(datos, how="left", left_on="nome", right_on="bairro")
 x_min, y_min, x_max, y_max = barrios.total_bounds
 
-fechas = datos_pm25['fecha'].unique()
-cmap = LinearSegmentedColormap.from_list("custom_green_red", ["#FFB6C1", "#8B0000"], N=256)
-
+fechas = datos['fecha'].unique()
+fechas = datos['ano'].unique()
 
 for fecha in fechas:
     # Filtrar datos para un solo día
-    datos_dia = barrios[barrios['fecha'] == fecha]
+    #datos_dia = barrios[barrios['fecha'] == fecha]
+    datos_dia = barrios[barrios['ano'] == fecha]
     
     # Crear el mapa
+    '''
     fig, ax = plt.subplots(1, 1, figsize=(10, 6))
     datos_dia.plot(column='pm25', ax=ax, legend=True, cmap='OrRd',
                    vmin=6, vmax=18,  # Fijar los valores mínimo y máximo de la escala de colores
                    legend_kwds={'label': "Concentração de PM2.5", 'orientation': "horizontal"})
-    ax.set_title(f'PM2.5 {fecha.strftime("%Y-%m-%d")}')
+    #ax.set_title(f'PM2.5 {fecha.strftime("%Y-%m-%d")}')
+    ax.set_title(f'PM2.5 {fecha}')
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_xlabel('')
     ax.set_ylabel('')
-    plt.savefig(f'mapa_{fecha.strftime("%Y-%m-%d")}.png')
+    #plt.savefig(f'mapa_{fecha.strftime("%Y-%m-%d")}.png')
+    plt.savefig(f'mapa_{fecha}.png')
     plt.close()
-    
+    '''
+        
+    # Crear el mapa compuesto
+    fig, axs = plt.subplots(1, 3, figsize=(18, 6))  # 3 subplots en una fila
+
+    # Mapa para PM2.5
+    datos_dia.plot(column='pm25', ax=axs[0], legend=True, cmap='OrRd',
+                    vmin=10, vmax=16, 
+                    #vmin=datos_dia['pm25'].min(), vmax=datos_dia['pm25'].max(),
+                    legend_kwds={'label': "Concentração de PM2.5", 'orientation': "horizontal"})
+    axs[0].set_title(f'PM2.5 {fecha}')
+    axs[0].set_xlim(x_min, x_max)
+    axs[0].set_ylim(y_min, y_max)
+    axs[0].set_xticks([])
+    axs[0].set_yticks([])
+    axs[0].set_xlabel('')
+    axs[0].set_ylabel('')
+
+    # Mapa para IVS
+    datos_dia.plot(column='ivs', ax=axs[1], legend=True, cmap='Blues',
+                    vmin=0, vmax=0.9,
+                    legend_kwds={'label': "Índice de Vulnerabilidade Social", 'orientation': "horizontal"})
+    axs[1].set_title(f'IVS {fecha}')
+    axs[1].set_xlim(x_min, x_max)
+    axs[1].set_ylim(y_min, y_max)
+    axs[1].set_xticks([])
+    axs[1].set_yticks([])
+    axs[1].set_xlabel('')
+    axs[1].set_ylabel('')
+
+    # Mapa para Locais
+    datos_dia.plot(column='locais', ax=axs[2], legend=True, cmap='Greens',
+                    vmin=0, vmax=600,
+                    legend_kwds={'label': "Atacados", 'orientation': "horizontal"})
+    axs[2].set_title(f'Locais {fecha}')
+    axs[2].set_xlim(x_min, x_max)
+    axs[2].set_ylim(y_min, y_max)
+    axs[2].set_xticks([])
+    axs[2].set_yticks([])
+    axs[2].set_xlabel('')
+    axs[2].set_ylabel('')
+    plt.savefig(f'mapa_composto_{fecha}.png')
+    plt.close()
 
 # Directorio donde están tus imágenes
-directorio_imagenes = 'mapas'
+directorio_imagenes = 'mapas2'
 # Obtiene una lista de archivos y los ordena
-archivos_imagenes = sorted(glob.glob(os.path.join(directorio_imagenes, 'mapa_*.png')))
+archivos_imagenes = sorted(glob.glob(os.path.join(directorio_imagenes, 'mapa_composto_*.png')))
 
 # Asume que todas las imágenes tienen el mismo tamaño
 imagen_ejemplo = cv2.imread(archivos_imagenes[0])
@@ -64,7 +107,7 @@ altura, ancho, capas = imagen_ejemplo.shape
 
 # Define el codec y crea el objeto VideoWriter
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')  # También puedes usar 'XVID'
-salida_video = cv2.VideoWriter('video_pm25.mp4', fourcc, 5, (ancho, altura))
+salida_video = cv2.VideoWriter('video_consolidado.mp4', fourcc, 1, (ancho, altura))
 
 
 # Lee cada imagen y la añade al video
